3df7d7ba1514eb75ba65c639e4a84a5d328a98ec
LevinIgor <igorlevin000@gmail.com>
2023-01-22 13:29:41 +0200

    .

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,33 +2,11 @@
 const btns = document.querySelectorAll('.checkbox__btn');
 
 btns.forEach(checkbox__btn => {
-    checkbox__btn.addEventListener('click',
-    function() {
+    checkbox__btn.addEventListener('click', function() {
         btns.forEach(elem =>
-    elem.classList.remove('active'));
+            elem.classList.remove('active'));
         this.classList.add('active');
     });
 });
 
 
-// Slider
-let offset = 0;
-const sliderLine = document.querySelector('.tranding__slider');
-
-document.querySelector('.next').addEventListener('click', 
-function(){
-    offset += 400;
-    if(offset > 800) {
-        offset = 0;
-    }
-    sliderLine.style.left = -offset + 'px';
-});
-
-document.querySelector('.back').addEventListener('click', 
-function(){
-    offset -= 400;
-    if(offset < 0) {
-        offset = 800;
-    }
-    sliderLine.style.left = -offset + 'px';
-});
